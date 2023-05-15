8c1f82bc688c72310a04632f70ef85dd03e7fe23
IvanKornieiev <bomak54@gmail.com>
2023-05-15 14:36:01 +0300

    add ProductDetailsPage

---
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Helmet} from "react-helmet";
+
+const ProductDetailsPage: React.FC = () => {
+  return <>
+    <Helmet>
+      <title>Page Product</title>
+    </Helmet>
+  </>
+};
+export default ProductDetailsPage;
