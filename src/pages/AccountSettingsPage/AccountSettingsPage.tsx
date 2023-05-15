405685abbc67e82eb4e5c97a599455c67fc382f7
IvanKornieiev <bomak54@gmail.com>
2023-05-15 14:23:44 +0300

    add AccountSettingsPage

---
+++ src/pages/AccountSettingsPage/AccountSettingsPage.tsx
@@ -0,0 +1,15 @@
+import {Helmet} from 'react-helmet'
+import React from "react";
+
+
+const AccountSettingsPage: React.FC = () => {
+  return <>
+    <Helmet>
+      <title>Настройки аккаунта - MW Marketplace</title>
+    </Helmet>
+
+    <h1>Настройки аккаунта</h1>
+  </>
+}
+
+export default AccountSettingsPage;
