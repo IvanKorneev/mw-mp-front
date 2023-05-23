9f3f58b2920e137de3ae84778174280cff6b87bf
IvanKornieiev <bomak54@gmail.com>
2023-05-23 16:31:36 +0300

    update Header add LeftSide  SearchWrapper RightSide

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,10 +1,61 @@
 import React from "react";
-import {Wrapper} from "./styled";
+import {
+  BtnCart,
+  BtnFavorites,
+  BtnNotifications,
+  BtnOrders,
+  BtnSearch,
+  Burger,
+  LeftSide,
+  Logo,
+  RightSide,
+  SearchWrapper,
+  Wrapper
+} from "./styled";
+import {paths} from 'routes/helpers'
+import Button from "../../components/Button";
+import {Link} from "react-router-dom";
+import { useSelector } from 'react-redux';
+import {selectIsLogged} from "../App/selectors";
+import UserDropdownMenu from './UserDropdownMenu';
+
+
+
 
 const Header: React.FC = () => {
+  const isLogged = useSelector(selectIsLogged)
   return (
     <Wrapper>
-      Header
+      <LeftSide>
+        <h1>Logo </h1>
+        <Button>
+          <Burger>
+            <div></div>
+            <div></div>
+          </Burger>
+          <span>Catalog</span>
+        </Button>
+      </LeftSide>
+      <SearchWrapper>
+        <input placeholder='product search'/>
+        <BtnSearch/>
+      </SearchWrapper>
+      <RightSide>
+        {
+          isLogged ? <>
+            <BtnOrders count={4}/>
+            <BtnFavorites count={5}/>
+            <BtnNotifications count={5} />
+            <BtnCart  count={5}/>
+            <UserDropdownMenu />
+          </> : (
+            <Link to={paths.login}>
+              &nbsp;&nbsp;&nbsp;
+              Sing In
+            </Link>
+          )
+        }
+      </RightSide>
     </Wrapper>
 
   );
