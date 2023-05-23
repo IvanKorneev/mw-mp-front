1dcf7105442f2c2f6c552c8e8d79de203ce31c4e
IvanKornieiev <bomak54@gmail.com>
2023-05-23 16:26:53 +0300

    add UserAvatar

---
+++ src/features/Header/UserAvatar.tsx
@@ -0,0 +1,15 @@
+import { UserAvatarWrapper } from './styled'
+import React from "react";
+
+
+interface I_DropdownPanelProps {
+  onClick: () => void
+}
+
+const UserAvatar: React.FC<I_DropdownPanelProps> = (props) => (
+  <UserAvatarWrapper onClick={props.onClick}>
+    <img src='https://i.pravatar.cc/46' alt='User avatar' />
+  </UserAvatarWrapper>
+)
+
+export default UserAvatar
